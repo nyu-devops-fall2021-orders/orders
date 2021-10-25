ee28ad7ad3c7ad935a97c5acc3bf942dc14781e0
abhishek-rr <ranjaneabhishek@hotmail.com>
2021-10-24 22:12:22 -0700

    created create functions for bot order and order items

--- service/routes.py
+++ service/routes.py
@@ -1,6 +1,6 @@
 from . import app
-from flask import Flask, jsonify#, request, url_for, make_response, abort
-from service.models import Order
+from flask import Flask, jsonify, request#, url_for, make_response, abort
+from service.models import OrderItem, Order
 #@app.route('/index')
 @app.route('/')
 
@@ -18,4 +18,37 @@ def index():
 def init_db():
     """ Initialies the SQLAlchemy app """
     global app
+    OrderItem.init_db(app)
     Order.init_db(app)
+
+@app.route("/createitem", methods=["POST"])
+def create_item():
+    """
+    Creates a Pet
+    This endpoint will create a Pet based the data in the body that is posted
+    """
+    app.logger.info("Request to create an order")
+    orderItem = OrderItem()
+    orderItem.deserialize(request.get_json())
+   
+    return str(orderItem.price)
+
+@app.route("/create", methods=["POST"])
+def create_order():
+    """
+    Creates a Pet
+    This endpoint will create a Pet based the data in the body that is posted
+    """
+    app.logger.info("Request to create an order")
+    #check_content_type("application/json")
+
+    order = Order()
+    order.deserialize(request.get_json())
+    order.create()
+    
+    return str(order.updated_at)
+
+    #return str(type(order.created_at))
+    #"make_response(
+    #    jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    #)"
