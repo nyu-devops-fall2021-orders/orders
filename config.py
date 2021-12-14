7c83209615439a6cff24f71d81a7d3157b370c53
Shantanu Dahiya <shantanu.webmail@gmail.com>
2021-12-14 16:00:02 -0500

    removed vcap from cloud

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ DATABASE_URI = os.getenv(
 )
 
 # override if we are running in Cloud Foundry
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.environ['VCAP_SERVICES'])
-    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+# if 'VCAP_SERVICES' in os.environ:
+#     vcap = json.loads(os.environ['VCAP_SERVICES'])
+#     DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
